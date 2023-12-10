ea06bcef8edbc705bdb572a3ab682af9d3aba3a3
William Joannes <william.joannes@outlook.com>
2023-12-11 00:27:18 +0100

    fjeioj

--- sncf.js
+++ sncf.js
@@ -6,11 +6,22 @@ const baseURL = 'https://api.navitia.io/v1/coverage/sncf/';
 
 // const from = "gare de l'est";
 // const to = "metz ville";
-const from = "orleans";
-const to = "dijon";
+const from = "part dieu";
+const to = "bruxelles";
+const date = '28/12/2023-11:30';
+
+
+function convertDateFormat(inputDate) {
+    const dateObject = moment(inputDate, 'DD/MM/YYYY-HH:mm');
+    const formattedDate = dateObject.format('YYYYMMDDTHHmmss');
+    return formattedDate;
+}
+
 
 const fetchPlaces = (name) => {
-    return fetch(`${baseURL}places/?q=${name}`, {
+    const url = `${baseURL}places/?q=${name}`;
+    //console.log(url);
+    return fetch(url, {
         headers: { 'Authorization': api_key }
     })
     .then(resp => resp.json());
@@ -18,20 +29,35 @@ const fetchPlaces = (name) => {
 
 Promise.all([fetchPlaces(from), fetchPlaces(to)])
     .then(data => {
-        const [dataFrom, dataTo] = data;
+        const [dataFrom, dataTo] = data; 
+        const fromStopAreas = dataFrom.places.filter(p => p.embedded_type === "stop_area");
+        const toStopAreas = dataTo.places.filter(p => p.embedded_type === "stop_area");
 
-        console.log('from: ', dataFrom.places[0].name);
-        console.log('to: ', dataTo.places[0].name);
+        if (fromStopAreas.length === 0 || toStopAreas.length === 0){
+            console.log('no journey found');
+            return;
+        }
 
-        fromId = dataFrom.places[0].id;
-        toId = dataTo.places[0].id;
-        const fullURL = `${baseURL}journeys/?from=${fromId}&to=${toId}`;
-        //console.log(fullURL);
-        fetch(fullURL, {
+        console.log('from: ', fromStopAreas[0].name);
+        console.log('to: ', toStopAreas[0].name);
+        fromId = fromStopAreas[0].id;
+        //console.log('fromId: ', fromId);
+        toId = toStopAreas[0].id;
+        //console.log('toId: ', toId);
+        const dateTime = convertDateFormat(date);
+        const url = `${baseURL}journeys/?from=${fromId}&to=${toId}&datetime=${dateTime}`;
+        //console.log(url);
+        fetch(url, {
             headers: { 'Authorization': api_key }
         })
         .then(resp => resp.json())
         .then(data => {
+            if (Object.keys(data).includes('error')){
+                console.log('ERROR (probably due to unavalable date)');
+                return;
+                // On dirait qu'on a seulement acces au horaires 
+                // quelques jours autours de la date actuelle :(
+            }
             console.log('journeys: ', data.journeys.map(j => ({
                 departure: moment(j.departure_date_time, 'YYYYMMDDTHHmm').format('MMMM D, YYYY, HH:mm'),
                 arrival: moment(j.arrival_date_time, 'YYYYMMDDTHHmm').format('MMMM D, YYYY, HH:mm'),
