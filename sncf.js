0e1af7c3c6c8dceeda452b9fd285fce152cf4efa
Marion <marionbailleux07@gmail.com>
2023-12-11 18:32:08 +0100

    sncf

--- sncf.js
+++ sncf.js
@@ -17,51 +17,21 @@ function convertDateFormat(inputDate) {
 }
 
 const fetchPlaces = (name) => {
-    const url = `${baseURL}places/?q=${name}`;
-    console.log(url);
-    return fetch(url, {
-        headers: { 'Authorization': api_key }
-    })
-    .then(resp => resp.json());
+  const url = `${baseURL}places/?q=${name}`;
+  //console.log(url);
+  return fetch(url, {
+    headers: { Authorization: api_key },
+  }).then((resp) => resp.json());
 };
 
-Promise.all([fetchPlaces(from), fetchPlaces(to)])
-    .then(data => {
-        const [dataFrom, dataTo] = data; 
-        const fromStopAreas = dataFrom.places.filter(p => p.embedded_type === "stop_area");
-        const toStopAreas = dataTo.places.filter(p => p.embedded_type === "stop_area");
-
-        if (fromStopAreas.length === 0 || toStopAreas.length === 0){
-            console.log('no journey found');
-            return;
-        }
-
-        console.log('from: ', fromStopAreas[0].name);
-        console.log('to: ', toStopAreas[0].name);
-        fromId = fromStopAreas[0].id;
-        //console.log('fromId: ', fromId);
-        toId = toStopAreas[0].id;
-        //console.log('toId: ', toId);
-        const dateTime = convertDateFormat(date);
-        const url = `${baseURL}journeys/?from=${fromId}&to=${toId}&datetime=${dateTime}`;
-        console.log(url);
-        fetch(url, {
-            headers: { 'Authorization': api_key }
-        })
-        .then(resp => resp.json())
-        .then(data => {
-            if (Object.keys(data).includes('error')){
-                console.log('ERROR (probably due to unavalable date)');
-                return;
-                // On dirait qu'on a seulement acces au horaires 
-                // quelques jours autours de la date actuelle :(
-            }
-            console.log('journeys: ', data.journeys.map(j => ({
-                departure: moment(j.departure_date_time, 'YYYYMMDDTHHmm').format('MMMM D, YYYY, HH:mm'),
-                arrival: moment(j.arrival_date_time, 'YYYYMMDDTHHmm').format('MMMM D, YYYY, HH:mm'),
-            })));
-        });
-    })
+Promise.all([fetchPlaces(from), fetchPlaces(to)]).then((data) => {
+  const [dataFrom, dataTo] = data;
+  const fromStopAreas = dataFrom.places.filter(
+    (p) => p.embedded_type === "stop_area"
+  );
+  const toStopAreas = dataTo.places.filter(
+    (p) => p.embedded_type === "stop_area"
+  );
 
   if (fromStopAreas.length === 0 || toStopAreas.length === 0) {
     console.log("no journey found");
