329c1b097932f5bc77568b73c20d53c2600eac4c
William Joannes <william.joannes@outlook.com>
2023-12-12 09:41:38 +0100

    cette fois la route users signing vraiment en post

--- routes/users.js
+++ routes/users.js
@@ -44,7 +44,7 @@ router.post("/signup", (req, res) => {
   });
 });
 
-router.get("/signin", (req, res) => {
+router.post("/signin", (req, res) => {
   if (!checkBody(req.body, ["email", "password"])) {
     res.json({ result: false, error: "Missing or empty fields" });
     return;
