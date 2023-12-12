e3257325eef11e18b2cc2228a323ea9d4864449b
Marion <marionbailleux07@gmail.com>
2023-12-12 17:57:17 +0100

    accomodaion json

--- routes/trips.js
+++ routes/trips.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var router = express.Router();
 const AccomodationSlot = require("../database/models/accommodation/accommodationSlots");
+const AccomodationExtras = require("../database/models/accommodation/accommodationExtras");
 
 router.get("/newAccomodation", (req, res) => {});
+
+router.get("/getExtras", (req, res) => {
+  AccomodationExtras.find().then((data) => {
+    let result = data.filter((e) => e.name !== "example");
+
+    return res.json(result);
+  });
+});
 
 module.exports = router;
