aba5a115f67193717a78159671f74e96d9d58131
William Joannes <william.joannes@outlook.com>
2023-12-13 09:32:53 +0100

    refait le connection.js avec le dotenv au debut

---
+++ database/connection.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+require('mongoose').connect(process.env.DB_CONNECTION_STRING, {
+    connectTimeoutMS: 5000
+})
+.then(() => console.log('connected to database'))
+.catch(() => console.log('failed to connect to database'));
