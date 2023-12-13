6b0e71baddc4df7c3f3b648b9b9c0fd8e5d4d67b
Louis <sanson.louis@gmail.com>
2023-12-13 16:36:30 +0100

    start generate route

--- routes/trips.js
+++ routes/trips.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
-const { saveTrip } = require("./savedtrips");
+// const { saveTrip } = require("./savedtrips");
 
-const Trip = require("../models/trip");
-const AccommodationSlot = require("../models/accommodationSlot");
+const Trip = require("../database/models/trips");
+const AccommodationSlot = require("../database/models/accommodation/accommodationRooms");
+const Destination = require("../database/models/destinations");
 
 const { tripA, tripB } = require("../exampleTrips");
 let trips = [tripA, tripB];
@@ -49,10 +50,52 @@ router.get(
 
 router.get("/newTransport", async (req, res) => {});
 
+const calculateDistance = (lat1, lon1, lat2, lon2) => {
+  const R = 6371; // Radius of the Earth in kilometers
+  const dLat = (lat2 - lat1) * (Math.PI / 180);
+  const dLon = (lon2 - lon1) * (Math.PI / 180);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1 * (Math.PI / 180)) *
+      Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c; // Distance in kilometers
+
+  return distance.toFixed(2);
+};
+
 //
-router.get("/generate", async (req, res) => {
-  // c'est ici qu'on creer tripA et tripB
+router.post("/generate", async (req, res) => {
+  const filters = req.body;
+  //Cette première section permet de donner le lieu de départ (aéroport, gare, etc.) le plus proche de
+  //l'adresse de l'utilisateur renseignée + de générer aléatoirement un lieu d'arrivée.
+  const data = await Destination.find();
+
+  const locations = data.map((e) => {
+    return {
+      name: e.name,
+      lat: e.centerLocation.latitude,
+      lon: e.centerLocation.longitude,
+      distance: calculateDistance(
+        e.centerLocation.latitude,
+        e.centerLocation.longitude,
+        filters.latitude,
+        filters.longitude
+      ),
+    };
+  });
+  const sortedlocations = locations.sort((a, b) => {
+    return a.distance - b.distance;
+  });
+  const destinationIndex =
+    Math.floor(Math.random() * sortedlocations.length) + 1;
+  const destination = sortedlocations[destinationIndex];
+  const departureLocation = sortedlocations[0];
+  // fin de la première section
+
+  return;
 });
 
-module.exports = router;
-module.exports = { tripA, tripB };
+(module.exports = router), { tripA, tripB };
