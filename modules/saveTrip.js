a013bcca514e7e27a92638d3752b4c8b5561b1bf
Marion <marionbailleux07@gmail.com>
2023-12-22 11:46:15 +0100

    gh

--- modules/saveTrip.js
+++ modules/saveTrip.js
@@ -2,55 +2,58 @@ const Trip = require("../database/models/trips");
 const User = require("../database/models/users");
 // const { tripA, tripB } = require("../exampleTrips");
 // let trips = [tripA, tripB];
-const { getTrips } = require('../routes/trips');
+const { getTrips } = require("../routes/trips");
 
 async function saveTrip(req) {
   const selectedTripIndex = Number(req.params.tripIndex);
   const userToken = req.params.userToken;
-  
+
   const trips = getTrips();
-  console.log('###################', trips);
-  const user = await User.find({token: userToken});
+  console.log("###################", trips);
+  const user = await User.find({ token: userToken });
   const tripForDB = getFormattedTripForDB(trips[selectedTripIndex], user._id);
   const newTrip = new Trip(tripForDB);
   const savedTrip = await newTrip.save();
   return { userToken, savedTrip };
 }
 
-function getFormattedTripForDB(trip, userId){
-
+function getFormattedTripForDB(trip, userId) {
   const activitiesDB = trip.activities.map((act) => {
-    return ({
-      activitySlot: act._id,  // activity slot id
-      activityExtras: [],  // no extras for now
-    });
+    return {
+      activitySlot: act._id, // activity slot id
+      activityExtras: [], // no extras for now
+    };
   });
 
-  return (
-    {
-      user: userId,
-      nbOfTravelers: trip.numberOfTravelers,
-      destination: trip.destination.id,
-      outboundJourney: {
-        transportSlot: 'the_slot_id', // put transport slot id here
-        transportExtras: [],  // no extras for now
-        seats: { from: 'seatNumber', to: 'seatNumber'}, // put seat numbers here
-        class: trip.outboundJourney.class,
-      },
-      inboundJourney: {
-        transportSlot: 'the_slot_id', // put transport slot id here
-        transportExtras: [],  // no extras for now
-        seats: { from: 'seatNumber', to: 'seatNumber'}, // put seat numbers here
-        class: trip.inboundJourney.class,
-      },
-      accommodation: {
-        accommodationRoom: trip.accommodation._id, // accommodation room id
-        accommodationExtras: trip.accommodation.accommodationBase.possibleExtras,
-      },
-      activities: activitiesDB,
-      totalPaidAmount: trip.total,
-    }
-  );
+  return {
+    user: userId,
+    nbOfTravelers: trip.numberOfTravelers,
+    destination: trip.destination.id,
+    outboundJourney: {
+      transportSlot: trip.outboundJourney.id, // put transport slot id here
+      transportExtras: [], // no extras for now
+      seats: {
+        from: trip.outboundJourney.seats.from,
+        to: trip.outboundJourney.seats.to,
+      }, // put seat numbers here
+      class: trip.outboundJourney.class,
+    },
+    inboundJourney: {
+      transportSlot: trip.inboundJourney.id, // put transport slot id here
+      transportExtras: [], // no extras for now
+      seats: {
+        from: trip.inboundJourney.seats.from,
+        to: trip.inboundJourney.seats.to,
+      }, // put seat numbers here
+      class: trip.inboundJourney.class,
+    },
+    accommodation: {
+      accommodationRoom: trip.accommodation._id, // accommodation room id
+      accommodationExtras: trip.accommodation.accommodationBase.possibleExtras,
+    },
+    activities: activitiesDB,
+    totalPaidAmount: trip.total,
+  };
 }
 
 async function getSavedTripById(tripId) {
@@ -58,7 +61,4 @@ async function getSavedTripById(tripId) {
   return savedTrip;
 }
 
-
 module.exports = { saveTrip, getFormattedTripForDB };
-
-
